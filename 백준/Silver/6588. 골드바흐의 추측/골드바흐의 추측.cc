87e10cdbe5f0668221f4bf787982db3e52a40b71
Muffinyj <102642248+Muffinyj@users.noreply.github.com>
2022-12-18 15:21:50 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 44 ms, Memory: 26436 KB -BaekjoonHub

---
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#define MAX 1000000
+
+using namespace std;
+
+vector<pair<int, int>> odd[MAX + 1];
+bool prime[MAX + 1];
+
+void getPrime() {
+	for (int i = 2; i <= sqrt(MAX); i++) {
+		for (int j = i * i; j <= MAX; j+=i) {
+			if (!prime[j]) {
+				prime[j] = true;
+			}
+		}
+	}
+}
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	getPrime();
+	int num;
+
+	while (1) {
+		cin >> num;
+		if (num == 0) {
+			break;
+		}
+		for (int i = 2; i <= MAX / 2; i++) {
+			if (!prime[i] && !prime[num - i]) {
+				cout << num << " = " << i << " + " << num - i << "\n";
+				break;
+			}
+		}
+	}
+
+	return 0;
+}
