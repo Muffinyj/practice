4f85b184fa48b28fbce3898401e223292337c8e7
Muffinyj <102642248+Muffinyj@users.noreply.github.com>
2022-12-23 23:35:26 +0900

    [Silver III] Title: N과 M (7), Time: 696 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+bool visited[9] = { false, };
+int arr[9] = { 0, };
+int v[9] = { 0, };
+
+void nm(int num, int index) {
+    if (index == m) {
+        for (int i = 0; i < m; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+    else {
+        for (int i = 0; i < n; i++) {
+            arr[index] = v[i];
+            nm(i, index + 1);
+        }
+    }
+}
+
+int main() {
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+
+    sort(v, v + n);
+
+    nm(0, 0);
+
+    return 0;
+}
